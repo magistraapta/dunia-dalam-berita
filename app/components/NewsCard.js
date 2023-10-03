7c7cdd4b38392eb9d5e7cf549132183205275231
Magistra <magistraagis@gmail.com>
2023-10-03 21:37:50 +0800

    fix/add href to detail news

--- app/components/NewsCard.js
+++ app/components/NewsCard.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Image from 'next/image';
 import newsCover from './image-example.webp';
 
-export default function NewsCard({title, desc}) {
+
+
+export default async function NewsCard({title, desc, thumbnail, link}) {
   return (
     <div className=' my-4 min-w-[400px] p-2 rounded-md '>
-      <a href='/detail'>
+      <a href={link}>
         <div className='w-full rounded-lg flex justify-center items-center'>
           <Image
             alt='news-image'
-            src={newsCover}
+            src={thumbnail}
             style={{
               maxWidth: '100%',
               height: 'auto',
             }}
+            width={400}
+            height={400}
             className=' rounded-md'
           />
         </div>
